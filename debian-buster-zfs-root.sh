b6de12f0c7ae90e88b2ba54e397cd1a561b5a080
kewiha <keithh@protonmail.ch>
2020-02-23 12:22:56 -0500

    Made the hostname customizable
    If NEWHOST is set at the top of the script, it will be used instead of generating a hostname based on hostid

--- debian-buster-zfs-root.sh
+++ debian-buster-zfs-root.sh
@@ -37,6 +37,8 @@ SIZESWAP=2G
 SIZETMP=3G
 SIZEVARTMP=3G
 
+NEWHOST="" #Manually specify hostname of new install, otherwise it will be generated
+
 ### User settings
 
 declare -A BYID
@@ -224,8 +226,9 @@ zpool status
 zfs list
 
 debootstrap --include=openssh-server,locales,linux-headers-amd64,linux-image-amd64,joe,rsync,sharutils,psmisc,htop,patch,less --components main,contrib,non-free $TARGETDIST /target http://deb.debian.org/debian/
-
-NEWHOST=debian-$(hostid)
+if [ "$NEWHOST" == "" ]; then
+	NEWHOST=debian-$(hostid)
+fi
 echo "$NEWHOST" >/target/etc/hostname
 sed -i "1s/^/127.0.1.1\t$NEWHOST\n/" /target/etc/hosts
 
